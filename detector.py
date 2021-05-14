061933545f1493547c3aa56b0f8ec91bdf720f8b
nilarnab <59472120+nilarnab@users.noreply.github.com>
2021-05-14 20:54:22 +0530

    Create detector.py

---
+++ detector.py
@@ -0,0 +1,61 @@
+# you might need to install this
+# pip install requests
+import requests
+
+from datetime import date
+import time
+
+# you might need to install this
+# pip install beeply
+# to learn more, you can go to https://www.geeksforgeeks.org/beeply-module-in-python/
+import winsound
+
+from os import system, name
+
+
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+
+today = date.today()
+d1 = today.strftime("%d-%m-%Y")
+
+print("DATE", d1)
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+r = requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=621&date=' + d1, headers = headers)
+
+print(r.status_code)
+
+iterations = 1
+
+while True:
+    found = False
+    if r.status_code == 200:
+        for center in r.json()['centers']:
+            for session in center['sessions']:
+                print(center['name'], ':', 'DATE: ', session['date'], 'age limit', session['min_age_limit'], 'capacity', session['available_capacity'])
+                if session['min_age_limit'] == 18 and session['available_capacity']:
+                    print('FOUND')
+                    found = True
+                else:
+                    pass
+    else:
+        print('MAYBE THEIR SERVER IS DOWN')
+
+    if found:
+        print("CALLING BEEP")
+
+        # frequency, duration(milisec)
+        winsound.Beep(800, 3000)
+    time.sleep(5)
+    print("ITERATIONS", iterations)
+    iterations += 1
+    clear()
+
